3ce83b3447536ea0fb76741de24cdf2d709612bd
Julius Vincent Babies <julius@familie-babies.de>
2023-06-06 20:51:16 +0200

    - VPLN-3 /gradle - JVM auf 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 kotlin {
     jvm {
-        jvmToolchain(11)
+        jvmToolchain(17)
         withJava()
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
